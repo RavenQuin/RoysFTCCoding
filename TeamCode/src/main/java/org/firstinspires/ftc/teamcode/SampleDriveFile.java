60b2c9602f6efc6f1290f1f35c2ac60bc25addf7
rmxams <rzhu825@gmail.com>
2023-10-20 15:02:44 -0400

    TFHFTY

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleDriveFile.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleDriveFile.java
@@ -11,6 +11,10 @@ public class SampleDriveFile extends LinearOpMode {
     DcMotor frontRight;
     DcMotor backLeft;
     DcMotor backRight;
+    DcMotor leftBase;
+    DcMotor leftHinge;
+    DcMotor rightBase;
+    DcMotor rightHinge
     public static double motorPow = 0.5;
 
     @Override
@@ -20,11 +24,15 @@ public class SampleDriveFile extends LinearOpMode {
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
+        leftBase = hardwareMap.get(DcMotor.class, "leftBase");
+        leftHinge = hardwareMap.get(DcMotor.class, "leftHinge");
+        rightBase = hardwareMap.get(DcMotor.class, "rightBase");
+        rightHinge = hardwareMap.get(DcMotor.class, "rightHinge");
         MecanumDrive drive = new MecanumDrive(frontLeft, frontRight, backLeft, backRight);
 
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
-
+//pee pee poo poo
         waitForStart();
         while(opModeIsActive()){
             drive.moveInTeleop(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x, motorPow);
